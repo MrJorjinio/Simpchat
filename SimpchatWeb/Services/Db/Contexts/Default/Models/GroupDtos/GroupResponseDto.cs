f8bc4f68fc692ee3cda3412bae5a3c7d24eb6f08
MrJorjinio <jorjiniomr@gmail.com>
2025-09-29 18:54:41 +0500

    Add user registration and group management features
    - Implemented `Register` method in `AuthController` for user registration.
    - Introduced `GroupController` with methods for creating groups, requiring authentication.
    - Updated `UserController` to enforce authorization on profile and password update methods.
    - Removed unique constraints on `Name` fields for `Groups` and `Channels` in the database.
    - Created migration to reflect changes in unique constraints.
    - Added AutoMapper profile for mapping between `Group` entities and DTOs.
    - Introduced `GroupCreateDto` and `GroupResponseDto` for group creation and response handling.

---
+++ SimpchatWeb/Services/Db/Contexts/Default/Models/GroupDtos/GroupResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpchatWeb.Services.Db.Contexts.Default.Models.GroupDtos
+{
+    public class GroupResponseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
