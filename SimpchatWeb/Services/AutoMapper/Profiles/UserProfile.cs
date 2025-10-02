c237e2cf4f65422b9163f4b58181816770379cbe
MrJorjinio <jorjiniomr@gmail.com>
2025-10-02 23:44:31 +0500

    Refactor user and chat management functionality
    - Updated UserController to modify authorization and add new user search functionality.
    - Made MediaUrl property in Message entity nullable.
    - Introduced new ChatController for handling chat messages and interactions.
    - Added migration to update database schema for nullable MediaUrl.
    - Created AutoMapper profiles for chat-related DTOs.
    - Introduced new DTO classes for improved data transfer in chat and user operations.

--- SimpchatWeb/Services/AutoMapper/Profiles/UserProfile.cs
+++ SimpchatWeb/Services/AutoMapper/Profiles/UserProfile.cs
@@ -14,10 +14,16 @@ namespace SimpchatWeb.Services.AutoMapper.Profiles
             CreateMap<User, UserRegisterPostDto>();
             CreateMap<User, UserResponseDto>();
             CreateMap<User, UserPutDto>();
+            CreateMap<User, UserSearchResponseDto>();
+            CreateMap<User, UserProfileGetResponseDto>()
+                .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.Id));
             CreateMap<UserLoginPostDto, User>();
             CreateMap<UserRegisterPostDto, User>();
             CreateMap<UserResponseDto, User>();
             CreateMap<UserPutDto, User>();
+            CreateMap<UserSearchResponseDto, User>();
+            CreateMap<UserProfileGetResponseDto, User>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.UserId));
         }
     }
 }
---
+++ SimpchatWeb/Services/Db/Contexts/Default/Models/ChatDtos/Temps/ChatMessageTempDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpchatWeb.Services.Db.Contexts.Default.Models.ChatDtos.Temps
+{
+    public class ChatMessageTempDto
+    {
+        public Guid ChatId { get; set; }
+        public Guid MessageId { get; set; }
+        public string Content { get; set; }
+        public bool IsSeen { get; set; }
+    }
+}
